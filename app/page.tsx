c3fa8903f0348b1dd7e66931d916cc6f580b821e
Sepehr Mohamadpor <sepehr1628@gmail.com>
2025-03-09 14:36:53 +0330

    feat: layout created and react icons package installed

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
+import Header from "@/app/_components/Header";
+import Main from "@/app/_components/Main";
+import Aside from "./_components/Aside";
+
 const Page: React.FC = async function () {
   return (
-    <main>
-      <h1>Hello World</h1>
-    </main>
+    <div className="grid grid-cols-[20rem_1fr] grid-rows-[auto_1fr] h-screen">
+      <div className="col-span-2">
+        <Header />
+      </div>
+
+      <Aside />
+      <Main />
+    </div>
   );
 };
 
